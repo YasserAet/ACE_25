cff160f691cc157d9cd4d70de317ea77721cd206
YasserAet <YasserAet>
2025-10-18 20:13:31 +0100

    TP4 hibernate done

---
+++ TP 4  Hibernate/TP4  Hibernate/src/main/java/entities/Salle.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    public Salle(String code, List<Machine> machines) {
+        this.code = code;
+        this.machines = machines;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
